659d770e222a2899765828d9bebd6af363d56c64
Allvess <patrickalves930@gmail.com>
2020-01-14 15:26:56 -0300

    criar setup

---
+++ setup.java
@@ -0,0 +1,17 @@
+public class setup {
+        public static void main(String[] args) {
+
+            int mouse = 250;
+            int teclado = 300;
+            int webcam = 250;
+            int gabineteFull = 5000;
+            int headset = 447;
+            double internet = 80.00;
+
+            double total = mouse + teclado + webcam + gabineteFull + headset + internet;
+            System.out.println ("Valor  do setup:" +  total  + "R$");
+            
+        }
+}
+
+
